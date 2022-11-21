b9f5a3565fc91a7000c284c76bb49db1d1b64fd1
Diwas Kumar Arya <diwasarya25@gmail.com>
2022-11-21 09:19:30 +0530

    Initial commit

---
+++ Leetcode_Questions/Easy/TwoSum.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twosm(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i);
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
+        TwoSum twosum = new TwoSum();
+        int[] arr = {2, 12, 9, 4, 1, 7, 8};
+        int target = 9;
+        int[] arr2 = twosum.twosm(arr, target);
+        System.out.println(Arrays.toString(arr2));
+    }
+}
---
+++ src/flow_of_programs_1/CheckLeapYear.java
@@ -0,0 +1,2 @@
+package flow_of_programs_1;public class CheckLeapYear {
+}
