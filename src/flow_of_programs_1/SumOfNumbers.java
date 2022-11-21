9c794c70a3043c157fc98b1835a3ff3a84830ece
Diwas Kumar Arya <diwasarya25@gmail.com>
2022-11-21 09:25:49 +0530

    Current Changes

--- src/flow_of_programs_1/SumOfNumbers.java
+++ src/flow_of_programs_1/SumOfNumbers.java
@@ -1,2 +1,10 @@
-package flow_of_programs_1;public class SumOfNumbers {
+package flow_of_programs_1;
+
+public class SumOfNumbers {
+    public static void main(String args[])
+    {
+        int n1 = 225, n2 = 115, sum;
+        sum = n1 + n2;
+        System.out.println("The sum of numbers is: "+sum);
+    }
 }
